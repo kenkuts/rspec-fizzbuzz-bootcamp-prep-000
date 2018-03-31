76c1305458cfa0299ad8a3d617d78d06b5af4dad
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-03-31 03:52:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,19 @@
-require 'pry'
+# require 'pry'
 
-def fizzbuzz(num)
-  binding.pry
+# def fizzbuzz(num)
+#   binding.pry
   
-  if num % 3 == 0 && num % 5 != 0
-    return 'Fizz'
-  elsif num % 3 != 0 && num % 5 == 0
-    return 'Buzz'
-  elsif num % 3 == 0 && num % 5 == 0
-    return "FizzBuzz"
-  else
-    return nil
-  end
+#   if num % 3 == 0 && num % 5 != 0
+#     return 'Fizz'
+#   elsif num % 3 != 0 && num % 5 == 0
+#     return 'Buzz'
+#   elsif num % 3 == 0 && num % 5 == 0
+#     return "FizzBuzz"
+#   else
+#     return nil
+#   end
+# end
+
+def fizzbuzz
+ 
 end
